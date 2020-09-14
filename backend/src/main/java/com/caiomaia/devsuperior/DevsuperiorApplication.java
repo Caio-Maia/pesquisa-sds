dd07659a21f157aae346ca8edf52114233635eae
Caio Jordan <caiojdlmaia@gmail.com>
2020-09-14 10:45:54 -0300

    Project Created

---
+++ backend/src/main/java/com/caiomaia/devsuperior/DevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.caiomaia.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/caiomaia/devsuperior/DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiomaia.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
