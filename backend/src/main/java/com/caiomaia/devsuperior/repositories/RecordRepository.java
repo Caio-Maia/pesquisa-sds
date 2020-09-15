47d2b8ea8538e73fe21890a2fddfc9ce5a40a933
Caio Maia <55146250+Caio-Maia@users.noreply.github.com>
2020-09-15 18:28:20 -0300

    Ready for production

--- backend/src/main/java/com/caiomaia/devsuperior/repositories/RecordRepository.java
+++ backend/src/main/java/com/caiomaia/devsuperior/repositories/RecordRepository.java
@@ -2,7 +2,6 @@ package com.caiomaia.devsuperior.repositories;
 
 import com.caiomaia.devsuperior.entities.Record;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
