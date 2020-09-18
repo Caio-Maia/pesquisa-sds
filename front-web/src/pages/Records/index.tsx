0db82a53338bb16a797eda3e2b35c8ccdf35b2e4
Caio Jordan <caiojdlmaia@gmail.com>
2020-09-17 22:00:10 -0300

    Criando página de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,31 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './styles.css'
+import { RecordResponse } from './types'
+import { formatDate } from './helpers'
+import Pagination from './Pagination'
+import { Link } from 'react-router-dom'
 
-const Records = () => (
-    <div className="page-container">
-        <table className="records-table" cellPadding="0" cellSpacing="0">
-            <thead>
-                <tr>
-                    <th>INSTANTE</th>
-                    <th>NOME</th>
-                    <th>IDADE</th>
-                    <th>PLATAFORMA</th>
-                    <th>GÊNERO</th>
-                    <th>TÍTULO DO GAME</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>12/06/2020</td>
-                    <td>Darigoberto</td>
-                    <td>123</td>
-                    <td>ps4</td>
-                    <td>ação</td>
-                    <td>Relampago marquinhos</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-);
+const BASE_URL = 'http://localhost:8080'
+
+const Records = () => {
+
+    const [ recordsResponse, setRecordResponse ] = useState<RecordResponse>();
+    const [activePage, setActivePage] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+            .then(response => setRecordResponse(response.data))
+    }, [activePage])
+
+    const handlerPageChange = (index: number) => {
+        setActivePage(index);
+    }
+
+    return (
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">
+                    GRAFICOSSS
+                    </button>
+                </Link>
+            </div>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => 
+                    <tr key={record.id} >
+                        <td>{formatDate(record.moment)}</td>
+                        <td>{record.name}</td>
+                        <td>{record.age}</td>
+                        <td className="text-secondary">{record.gamePlatform}</td>
+                        <td>{record.genreName}</td>
+                        <td className="text-primary">{record.gameTitle}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage} 
+                goToPage={handlerPageChange}
+                totalPages={recordsResponse?.totalPages}
+            />
+        </div>
+    );
+}
 
 export default Records;
